072956e7688b56661d95f86e0fbdb60f797b4221
Fabien PETIT <fab37100@gmail.com>
2021-02-27 23:12:10 +0100

    Integration de la configuration YAML

--- bot.js
+++ bot.js
@@ -1,17 +1,21 @@
 // Importation des dependances
 const Discord = require('discord.js');
 const Exec = require('ssh-exec');
-require('dotenv').config()
+const YAML = require('yaml');
+const fs = require('fs');
+require('dotenv').config();
 
-// Imppoortation des variables d'environnement
+// Importation des variables d'environnement
 const IDDISCORDCHANEL = process.env.IDDISCORDCHANEL;
+const CLIENTIDDISCORDBOT = process.env.CLIENTIDDISCORDBOT;
 const VALHEIMIPSERVER = process.env.VALHEIMIPSERVER;
 const VALHEIMUSER = process.env.VALHEIMUSER;
 const VALHEIMPWD = process.env.VALHEIMPWD;
 const TOKENDISCORDBOT = process.env.TOKENDISCORDBOT;
 
-// Création des variables
-const callBot = '$!';
+// Importation de la configuration
+const configBot = YAML.parse(fs.readFileSync('./config.yml', 'utf8'));
+console.log(configBot);
 
 const client = new Discord.Client();
 client.login(TOKENDISCORDBOT);
@@ -20,42 +24,47 @@ client.on('ready', () => {
     console.log('Discord bot ready!!');
 });
 
+function execCommand(msg, execConfig, user, ip, pwd) {
+    if (execConfig.command) {
+        msg.channel.send(execConfig.message_info);
+        Exec(execConfig.command, {
+            user:       user,
+            host:       ip,
+            password:   pwd
+        },function (err, stdout, stderr) {
+            if (err){
+                msg.channel.send("ERROR :" + err);
+                console.log("ERROR :", err, stdout, stderr);    
+            }
+            else{
+                msg.channel.send(execConfig.message_succes);
+            }
+        });
+    }else{
+        msg.channel.send("Humain ! Bois une bière patiemment en attendant que je te forge ce dons");
+    }
+}
+
+
+
 client.on('message', msg => {
-    console.log(msg.content);
-    if (msg.content === callBot+'hello' && msg.channel.id === IDDISCORDCHANEL) {
-        msg.channel.send('Par la puissance de mon sceptre, je te salue viking');
-    }
-    if (msg.content === callBot+'status' && msg.channel.id === IDDISCORDCHANEL) {
-        msg.channel.send('Fonction : status');
-        execCommand(msg,'./vhserver details',VALHEIMUSER,VALHEIMIPSERVER,VALHEIMPWD);
-    }
-    if (msg.content === callBot+'restart' && msg.channel.id === IDDISCORDCHANEL) {
-        msg.channel.send('Fonction : restart');
-        execCommand(msg,'./vhserver restart',VALHEIMUSER,VALHEIMIPSERVER,VALHEIMPWD);
-    }
-    if (msg.content === callBot+'update' && msg.channel.id === IDDISCORDCHANEL) {
-        msg.channel.send('Fonction : update');
-        execCommand(msg,'./vhserver update',VALHEIMUSER,VALHEIMIPSERVER,VALHEIMPWD);
-    }
-    if (msg.content === callBot+'reboot' && msg.channel.id === IDDISCORDCHANEL) {
-        msg.channel.send('Fonction : reboot');
-        execCommand(msg,'./vhserver reboot',VALHEIMUSER,VALHEIMIPSERVER,VALHEIMPWD);
+    if (msg.content.substring(0,configBot.bot.invokeKey.length) == configBot.bot.invokeKey && msg.channel.id === IDDISCORDCHANEL && ! (msg.author.id === CLIENTIDDISCORDBOT)){
+        console.log(msg.content);
+        if (msg.content === configBot.bot.invokeKey+'status') {
+            execCommand(msg,configBot.bot.features['status'],VALHEIMUSER,VALHEIMIPSERVER,VALHEIMPWD);
+        }
+        else if (msg.content === configBot.bot.invokeKey+'reboot') {
+            execCommand(msg,configBot.bot.features['reboot'],VALHEIMUSER,VALHEIMIPSERVER,VALHEIMPWD);
+        }
+        else if (msg.content === configBot.bot.invokeKey+'update') {
+            execCommand(msg,configBot.bot.features['update'],VALHEIMUSER,VALHEIMIPSERVER,VALHEIMPWD);
+        }
+        else{
+            msg.channel.send('Humain ! voici les pouvoirs que je te confère sur le monde de Valheim :');
+            for (const [key,value] of Object.entries(configBot.bot.features)){
+                msg.channel.send(`${configBot.bot.invokeKey}${key}: ${value.help}`);
+            }
+            console.log("NOBODY => HELP");
+        }
     }
 });
-
-
-function execCommand(msg, command, user, ip, pwd) {
-    Exec(command, {
-        user:       user,
-        host:       ip,
-        password:   pwd
-    },function (err, stdout, stderr) {
-        if (err){
-            msg.channel.send("l'erreur suivante est survenue :" + err);
-            console.log(err, stdout, stderr);    
-        }
-        // else{
-        //     msg.channel.send("retour suivant :" + stdout);
-        // }
-    });
-}
